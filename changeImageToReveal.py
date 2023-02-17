4dfcf1749c2e636fff213a81d4472d7fa8b2de19
ika9810 <ika7204@naver.com>
2023-02-17 13:42:31 +0900

    [UPD]Delete DNA with python code pop && Revert Legend Ratings to Reveal

--- changeImageToReveal.py
+++ changeImageToReveal.py
@@ -7,9 +7,15 @@ for i in count_JSON:
     try :
         with open("./json/" + i, "r", encoding="utf8", errors='ignore') as json_file:
             json_data = json.load(json_file)
+            #DNA 삭제하기
+            json_data.pop("dna")
             if int(i.split(".json")[0]) in [1, 10, 74, 279, 445, 567, 815, 936, 994, 1000]:
-                json_data["image"] = "https://shiningneko.metatory.co.kr/reveal/legend.png"
-                json_data["animation_url"] = "https://shiningneko.metatory.co.kr/reveal/legend_ticket.mp4"
+                #레전드로 리빌하기 코드 두줄
+                # json_data["image"] = "https://shiningneko.metatory.co.kr/reveal/legend.png"
+                # json_data["animation_url"] = "https://shiningneko.metatory.co.kr/reveal/legend_ticket.mp4"
+                #리빌로 에셋 링크 변경하기 코드 두줄
+                json_data["image"] = "https://shiningneko.metatory.co.kr/reveal/reveal.png"
+                json_data["animation_url"] = "https://shiningneko.metatory.co.kr/reveal/reveal_ticket.mp4"
             else:
                 json_data["image"] = "https://shiningneko.metatory.co.kr/reveal/reveal.png"
                 json_data["animation_url"] = "https://shiningneko.metatory.co.kr/reveal/reveal_ticket.mp4"
