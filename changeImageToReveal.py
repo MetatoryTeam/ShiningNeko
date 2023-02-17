828f0c7d3b6b7eb58966efeee99c376a005d009f
ika9810 <ika7204@naver.com>
2023-02-17 13:25:11 +0900

    [FINAL]Final version of 990 Normal Version, attributes is unknown

--- changeImageToReveal.py
+++ changeImageToReveal.py
@@ -7,8 +7,14 @@ for i in count_JSON:
     try :
         with open("./json/" + i, "r", encoding="utf8", errors='ignore') as json_file:
             json_data = json.load(json_file)
-            json_data["image"] = "https://shiningneko.metatory.co.kr/reveal/reveal.png"
-            json_data["animation_url"] = "https://shiningneko.metatory.co.kr/reveal/reveal_ticket.mp4"
+            if int(i.split(".json")[0]) in [1, 10, 74, 279, 445, 567, 815, 936, 994, 1000]:
+                json_data["image"] = "https://shiningneko.metatory.co.kr/reveal/legend.png"
+                json_data["animation_url"] = "https://shiningneko.metatory.co.kr/reveal/legend_ticket.mp4"
+            else:
+                json_data["image"] = "https://shiningneko.metatory.co.kr/reveal/reveal.png"
+                json_data["animation_url"] = "https://shiningneko.metatory.co.kr/reveal/reveal_ticket.mp4"
+            for j in range(len(json_data["attributes"])):
+                json_data["attributes"][j]["value"] = "Unknown"
             with open("./json/" + i, 'w', encoding="utf8", errors='ignore') as outfile:
                 json.dump(json_data, outfile, indent=4)
     except:
